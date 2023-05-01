ff328960d0ab2dd1939cce0becd6b9c5e064e771
Dialmee <maggie.fabre02@gmail.com>
2023-05-01 23:31:33 +0200

    c# exemple

---
+++ Assets/Maggie/script/exempleTempo.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DisplayTime : MonoBehaviour
+{
+
+    [SerializeField] private float ClickTime;
+    [SerializeField] private float ClickCoolDown;
+    [SerializeField] private float ClickWindow;
+
+    [SerializeField] private GameObject TimingSphere;
+    private float TimingSizeDivider;
+    [SerializeField] private TMP_Text Result;
+    [SerializeField] private TMP_Text TimingBool;
+
+    private bool InTiming;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClickTime = ClickCoolDown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TimingSizeDivider += Time.deltaTime;
+        TimingBool.text = InTiming.ToString();
+
+        TimingSphere.transform.localScale = new Vector3(1 - TimingSizeDivider,1 -TimingSizeDivider,1 -TimingSizeDivider) ;
+        if (Time.time >= ClickTime)
+        {
+            ClickTime += ClickCoolDown;
+            TimingSizeDivider = 0;
+        }
+
+        if (Time.time >= ClickTime - ClickWindow && Time.time <= ClickTime + ClickWindow)
+        {
+            InTiming = true;
+        }
+        else
+        {
+            InTiming = false;
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            if (InTiming)
+            {
+                Result.text = "Succès !";
+            }
+            else
+            {
+                Result.text = "Échec ...";
+            }
+        }
+    }
+
+
+}
